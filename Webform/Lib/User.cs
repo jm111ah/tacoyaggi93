ed7d31f8253dbbce6e6bff8678c675e2d5f62eab
tacoyaggi93 <tacoyaggi93@gmail.com>
2021-08-18 23:32:12 +0900

    [정성균] sqlcommand parameter

--- Webform/Lib/User.cs
+++ Webform/Lib/User.cs
@@ -9,45 +9,67 @@ namespace Webform2.Lib
     public class User
     {
         public string result { get; set; }
-        public string  sql { get; set; }
-        //public string connection = "Data Source=127.0.0.1,1433;Initial Catalog=Crown;User ID=sa;Password=!Wjd635933";
-        public SqlConnection con = new SqlConnection("Data Source=127.0.0.1,1433;Initial Catalog=Crown;User ID=sa;Password=!Wjd635933");
-        public string Login(string email , string password)
+        public string sql { get; set; }
+        public string error { get; set; }
+
+        
+        public string Login(string email, string password)
         {
-            sql = "select * from [User] where Email = '" + email + "' and Password = '" + password + "'";
-            //SqlConnection con = new SqlConnection();
+            sql = "select * from [User] where Email = @email and Password = @password";
+            using (SqlConnection con = new SqlConnection("Data Source=127.0.0.1,1433;Initial Catalog=Crown;User ID=sa;Password=!Wjd635933"))
+            {
+                SqlCommand cmd = new SqlCommand(sql, con);
+                cmd.Parameters.AddWithValue("@email", email);
+                cmd.Parameters.AddWithValue("@password", password);
+                try
+                {
+                    con.Open();
+                    SqlDataReader rdr = cmd.ExecuteReader();
+                    rdr.Read();
+                    if (!string.IsNullOrEmpty(rdr["Email"].ToString()))
+                    {
+                        result = "OK";
+                    }
+                    else
+                    {
+                        result = "NO";
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    error = ex.ToString();
+                    result = "NO";
+                }
+                // using을 사용하므로 굳이 종료를 해줄 필요 없다. 
+                //finally
+                //{
+                //    con.Close();
+                //}
+            }
+
+
+            return result;
+        }
+
+        public void Signup(string email, string name, string gender, string password)
+        {
+            sql = "insert into [User](Email,Password,Name,Gender) values('" + email + "','" + password + "','" + name + "','" + gender + "')";
             try
             {
-                //con = new SqlConnection(connection);
                 con.Open();
                 SqlCommand cmd = new SqlCommand(sql, con);
                 SqlDataReader rdr = cmd.ExecuteReader();
                 rdr.Read();
-                if (!string.IsNullOrEmpty(rdr["Eamil"].ToString()))
-                {
-                    result = "OK";
-                }
-                else
-                {
-                    result = "NO";
-                }
-
             }
             catch (Exception ex)
             {
-                result = "NO";
+                error = ex.ToString();
             }
             finally
             {
                 con.Close();
             }
-
-            return result;
-        }
-
-        public void Signup(string email,string name,string gender,string password)
-        {
-            sql = "insert into [User](Email,Password,Name,Gender) values('"+email+"','"+password+"','"+name+"','"+gender+"')";
         }
     }
 }
--- Webform/Login.aspx.cs
+++ Webform/Login.aspx.cs
@@ -21,36 +21,6 @@ namespace Webform2
 
             User user = new User();
             result = user.Login(email,password);
-
-            //connection = "Data Source=127.0.0.1,1433;Initial Catalog=Crown;User ID=sa;Password=!Wjd635933";
-            //sql = "select * from [User] where Email = '" + email + "' and Password = '" + password + "'";
-            //SqlConnection con = new SqlConnection();
-
-            //try
-            //{
-            //    con = new SqlConnection(connection);
-            //    con.Open();
-            //    SqlCommand cmd = new SqlCommand(sql,con);
-            //    SqlDataReader rdr = cmd.ExecuteReader();
-            //    rdr.Read();
-            //    if (!string.IsNullOrEmpty(rdr["Eamil"].ToString()))
-            //    {
-            //        result = "OK";
-            //    }
-            //    else
-            //    {
-            //        result = "NO";
-            //    }
-                
-            //}
-            //catch(Exception ex)
-            //{
-            //    result = "NO";
-            //}
-            //finally
-            //{
-            //    con.Close();
-            //}
         }
     }
 }
