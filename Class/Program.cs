ed0432fd649190300a231754e84b7ea0dc6a4682
tacoyaggi93 <tacoyaggi93@triptopaz.com>
2021-10-07 17:58:53 +0900

    dd

---
+++ Class/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Class
+{
+    class Test
+    {
+        // 생성자
+        public Test()
+        {
+            this.City = "서울숲";
+        }
+
+        // 필드
+        public string Name = string.Empty;
+        public string City = string.Empty;
+        
+        // 메서드
+        public void Method(int a, int b) 
+        {
+            Console.WriteLine(String.Format("이름 : {0} // 나이 : {1} // 도시 : {2}" , this.Name , a+b , this.City));
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The number of processors " +
+                "on this computer is {0}.",
+                Environment.ProcessorCount);
+        }
+    }
+}
