f2b792d3ec02986d43739d822e32d84abff42b17
tacoyaggi93 <tacoyaggi93@gmail.com>
2021-12-06 14:26:41 +0900

    [정성균]
    20211206

---
+++ Crown/Controllers/CharpController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Crown.Controllers
+{
+    public class CharpController : Controller
+    {
+        public IActionResult Charp()
+        {
+            return View();
+        }
+
+        public IActionResult Charp2()
+        {
+            return View();
+        }
+        public IActionResult Charp3()
+        {
+            return View();
+        }
+        public IActionResult Charp4()
+        {
+            return View();
+        }
+        public IActionResult Charp5()
+        {
+            return View();
+        }
+        public IActionResult Charp6()
+        {
+            return View();
+        }
+        public IActionResult Charp7()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Crown/Controllers/OOPController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Crown.Controllers
+{
+    public class OOPController : Controller
+    {
+        public IActionResult OOP()
+        {
+            return View();
+        }
+    }
+}
