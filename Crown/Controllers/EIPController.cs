9000303227a25d14bbca8bbebddc86cf936e821c
tacoyaggi93 <tacoyaggi93@gmail.com>
2021-12-07 21:39:36 +0900

    [정성균]
    SDLC 2021-12-07

---
+++ Crown/Controllers/EIPController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Crown.Controllers
+{
+    public class EIPController : Controller
+    {
+        /*
+        Date Created : 2021-12-07
+        Writer : tacoyaggi93
+        Content : Software Development Life Cycle Model Info
+         */
+        public IActionResult SDLC()
+        {
+            return View();
+        }
+    }
+}
